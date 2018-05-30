05dc0020767b6e8cc2a728fc9935f3e4b457a518
Dmitry Bely <d.bely@recognize.ru>
2018-05-30 17:13:04 +0300

    Update package_folder-dependent absolute paths in /etc/fstab (otherwise downloaded binary package will fail)

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,7 +7,8 @@ import os
 
 
 class TestPackage(ConanFile):
-    default_options = "cygwin_installer:exclude_files=*/link.exe"
+    default_options = "cygwin_installer:no_acl=True", \
+                      "cygwin_installer:exclude_files=*/link.exe"
 
     def test(self):
         bash = tools.which("bash.exe")
@@ -17,6 +18,7 @@ class TestPackage(ConanFile):
         else:
             raise ConanException("No instance of bash.exe could be found on %PATH%")
 
+        self.run('cygcheck -c')
         self.run('bash.exe -c ^"uname -a^"')
         self.run('bash.exe -c ^"test -L /etc/networks^"')
         self.run('bash.exe -c ^"! test -f /bin/link"')
